9745f032ece68aa825c677fa3d820eddce6ac52f
jasim741891 <jasimgai74@gmail.com>
2021-08-25 19:54:44 +0600

    let and const solv number -1

---
+++ module31solve.js
@@ -0,0 +1,33 @@
+// 3 er a number solving
+var age = 20;
+if (age > 18) {
+    const name1 = 'akbor';
+    let name2 = 'khalel';
+    var name3 = 'jaleal';
+    // console.log(name1)
+    // console.log(name2)
+    // console.log(name3)
+}
+// console.log(name1)
+// console.log(name2)
+// console.log(name3)
+function chang() {
+    const name1 = 'akbor';
+    let name2 = 'khalel';
+    var name3 = 'jaleal';
+    // console.log(name1)
+    // console.log(name2)
+    // console.log(name3)
+}
+chang()
+// console.log(name1)
+// console.log(name2)
+// console.log(name3)
+
+
+// let number1 = 30
+// number1 = 40
+// console.log(number1)
+const number1 = 30
+number1 = 40
+console.log(number1)
